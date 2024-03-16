ff625134aed081dbb46ca277df0bc7d2462b1cff
MaxMexico <142561188+MaxMexico@users.noreply.github.com>
2024-03-16 12:21:16 +0100

    Add files via upload
    upload

--- Projet1.py
+++ Projet1.py
@@ -1,32 +1,28 @@
 import pandas as pd
-from gnews import NewsClient
+from gnews import GNews
 
 
-lang = "fr"
-
-nb_articles = 100
-
-
-client = NewsClient(lang=lang)
-
+client = GNews(language="fr", country="FR", period='7d', start_date=None, end_date=None, max_results=3)
 choix = input("Voulez-vous des actualités Top du moment (1) ou des actualités en rapport avec un mot-clé (2) ? ")
 
 if choix == "1":
-    articles = client.get_top_news(max_results=100)
+    articles = client.get_top_news()
 
 elif choix == "2":
     keyword = input("Entrez votre mot-clé : ")
-    articles = client.get_news(keyword, max_results=100)
+    articles = client.get_news(keyword)
 else:
     print("Choix invalide.")
     exit()
 
 df = pd.DataFrame({
-    "Titre": [article.title for article in articles],
-    "Lien": [article.url for article in articles],
-    "Date de publication": [article.publish_date for article in articles],
-    "Source": [article.source for article in articles],
-    "Résumé": [article.summary for article in articles]
+    "Titre": [article['title'].split(' - ')[0] for article in articles],
+    "Source": [article['title'].split(' - ')[1] for article in articles],
+    #"source2" : [article['publisher'] for article in articles], mauvais format "{'href': 'https://www.20minutes.fr'
+    "url" : [article['url'] for article in articles],
+    "published date": [article['published date']for article in articles],
+    "description": [article['description']for article in articles]
+
 })
 
 df.to_csv("news.csv", index=False)
